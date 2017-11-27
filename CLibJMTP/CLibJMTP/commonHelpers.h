39e56356b35ffd522e12cb970ae150e2506977cc
Greg Meltzer <vadercows@gmail.com>
2017-11-27 11:36:41 -0500

    needed fresh start

--- CLibJMTP/CLibJMTP/commonHelpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void wcsAllocCpy(wchar_t **destination, const wchar_t* source);
-
-const wchar_t pathDelimiter();
-
-struct cmp_wstr {
-	bool operator()(wchar_t const *a, wchar_t const *b) const;
-};
