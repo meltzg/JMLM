99c4e2085c613155f8a60ec9ee07dde7f23270ed
Gregory Meltzer <vadercows@gmail.com>
2018-12-24 19:06:18 -0500

    adding some starter stuff for javafx

---
+++ src/main/java/org/meltzg/jmlm/client/Launcher.java
@@ -0,0 +1,7 @@
+package org.meltzg.jmlm.client;
+
+public class Launcher {
+    public static void main(String[] args) {
+        LibrarySyncManager.main(args);
+    }
+}
---
+++ src/main/java/org/meltzg/jmlm/client/LibrarySyncManager.java
@@ -0,0 +1,25 @@
+package org.meltzg.jmlm.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LibrarySyncManager extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        var root = (Parent) FXMLLoader.load(getClass().getResource("LibrarySyncManager.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
