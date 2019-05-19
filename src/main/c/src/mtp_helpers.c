aa760e58793b14f50e38c0ae44a2f24856b72884
Gregory Meltzer <vadercows@gmail.com>
2019-05-18 20:02:59 -0400

    get raw deivce list while opening

--- src/main/c/src/mtp_helpers.c
+++ src/main/c/src/mtp_helpers.c
@@ -35,29 +35,31 @@ void toMTPDeviceInfo(MTPDeviceInfo *device_info, LIBMTP_mtpdevice_t *device, uin
     device_info->devNum = devNum;
 }
 
-bool getOpenDevice(MTPDeviceInfo *deviceInfo, const char *deviceId, LIBMTP_mtpdevice_t **device, uint32_t *busLocation, uint8_t *devNum)
+bool getOpenDevice(MTPDeviceInfo *deviceInfo, const char *deviceId, LIBMTP_mtpdevice_t **device, LIBMTP_raw_device_t **raw_devs, uint32_t *busLocation, uint8_t *devNum)
 {
-    LIBMTP_raw_device_t *raw_devs = NULL;
+    *device = NULL;
+    *raw_devs = NULL;
+    
     LIBMTP_mtpdevice_t *open_device = NULL;
     LIBMTP_error_number_t ret;
     int numdevs;
-    ret = LIBMTP_Detect_Raw_Devices(&raw_devs, &numdevs);
+    ret = LIBMTP_Detect_Raw_Devices(raw_devs, &numdevs);
     if (ret == LIBMTP_ERROR_NONE && numdevs > 0)
     {
         for (int i = 0; i < numdevs; i++)
         {
-            LIBMTP_mtpdevice_t *found_device = LIBMTP_Open_Raw_Device_Uncached(&raw_devs[i]);
+            LIBMTP_mtpdevice_t *found_device = LIBMTP_Open_Raw_Device_Uncached(raw_devs[i]);
             char *serial = LIBMTP_Get_Serialnumber(found_device);
             MTPDeviceIdInfo id_info;
-            id_info.vendor_id = raw_devs[i].device_entry.vendor_id;
-            id_info.product_id = raw_devs[i].device_entry.product_id;
+            id_info.vendor_id = (*raw_devs)[i].device_entry.vendor_id;
+            id_info.product_id = (*raw_devs)[i].device_entry.product_id;
             id_info.serial = serial;
 
             if (strcmp(toIDStr(id_info), deviceId) == 0)
             {
                 *device = found_device;
-                *busLocation = raw_devs[i].bus_location;
-                *devNum = raw_devs[i].devnum;
+                *busLocation = (*raw_devs)[i].bus_location;
+                *devNum = (*raw_devs)[i].devnum;
                 toMTPDeviceInfo(deviceInfo, found_device, *busLocation, *devNum, id_info.vendor_id, id_info.product_id);
                 // free(raw_devs);
                 return true;
@@ -93,10 +95,10 @@ int getDevicesInfo(MTPDeviceInfo **devices)
             LIBMTP_Release_Device(device);
         }
     }
-    if (raw_devs != NULL)
-    {
-        free(raw_devs);
-    }
+    // if (raw_devs != NULL)
+    // {
+    //     free(raw_devs);
+    // }
 
     return numdevs;
 }
@@ -104,77 +106,54 @@ int getDevicesInfo(MTPDeviceInfo **devices)
 bool getDeviceInfo(MTPDeviceInfo *deviceInfo, const char *deviceId)
 {
     LIBMTP_mtpdevice_t *device = NULL;
+    LIBMTP_raw_device_t *rawdevices = NULL;
     uint32_t busLocation;
     uint8_t devNum;
+    bool ret = false;
 
-    if (getOpenDevice(deviceInfo, deviceId, &device, &busLocation, &devNum))
+    if (getOpenDevice(deviceInfo, deviceId, &device, &rawdevices, &busLocation, &devNum))
     {
         LIBMTP_Release_Device(device);
-        return true;
+        ret = true;
     }
-
-    return false;
-}
-
-void initMTP() {
-    LIBMTP_Init();
-}
-
-static uint32_t
-lookup_folder_id(LIBMTP_folder_t *folder, char *path, char *parent)
-{
-    char *current;
-    uint32_t ret = (uint32_t)-1;
-
-    if (strcmp(path, "/") == 0)
-        return 0;
-
-    if (folder == NULL)
+    if (rawdevices != NULL)
     {
-        return ret;
+        free(rawdevices);
     }
-
-    current = malloc(strlen(parent) + strlen(folder->name) + 2);
-    sprintf(current, "%s/%s", parent, folder->name);
-    if (strcasecmp(path, current) == 0)
-    {
-        free(current);
-        return folder->folder_id;
-    }
-    if (strncasecmp(path, current, strlen(current)) == 0)
-    {
-        ret = lookup_folder_id(folder->child, path, current);
-    }
-    free(current);
-    if (ret != (uint32_t)(-1))
-    {
-        return ret;
-    }
-    ret = lookup_folder_id(folder->sibling, path, parent);
     return ret;
+}
+
+void initMTP()
+{
+    LIBMTP_Init();
 }
 
 bool getStorageDevice(MTPStorageDevice *storageDevice, const char *device_id, const char *path)
 {
-    LIBMTP_mtpdevice_t *device = NULL;
+    LIBMTP_mtpdevice_t *device;
+    LIBMTP_raw_device_t *rawdevices;
+
     uint32_t busLocation;
     uint8_t devNum;
     MTPDeviceInfo deviceInfo;
+    bool ret = false;
 
-    if (!getOpenDevice(&deviceInfo, device_id, &device, &busLocation, &devNum))
+    if (getOpenDevice(&deviceInfo, device_id, &device, &rawdevices, &busLocation, &devNum))
     {
-        return false;
+        LIBMTP_folder_t *folders = LIBMTP_Get_Folder_List(device);
+        printf("folders is null %d\n", folders == NULL);
+
+        storageDevice->storage_id = NULL;
+        storageDevice->capacity = 0;
+        storageDevice->free_space = 0;
+
+        LIBMTP_Release_Device(device);
     }
 
-    LIBMTP_folder_t *folders = LIBMTP_Get_Folder_List(device);
+    if (rawdevices != NULL)
+    {
+        free(rawdevices);
+    }
 
-    uint32_t folderId = lookup_folder_id(folders, path, "");
-    printf("Folder ID %d\n", folderId);
-
-    storageDevice->storage_id = NULL;
-    storageDevice->capacity = 0;
-    storageDevice->free_space = 0;
-
-    LIBMTP_Release_Device(device);
-    return true;
+    return ret;
 }
